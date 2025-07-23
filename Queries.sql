ad69d82390191910fba2cde96ea41fb13acd55dd
Minna Fathima <112232867+MinnaFathima@users.noreply.github.com>
2025-07-23 18:28:37 +0530

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,15 @@
+USE ecommerce;
+
+SHOW TABLES;
+
+SELECT * FROM adsalesandmetrics;
+
+SELECT * FROM eligibilitytable;
+
+SELECT * FROM totalsalesandmetrics;
+
+SHOW COLUMNS FROM adsalesandmetrics;
+
+SHOW COLUMNS FROM eligibilitytable;
+
+SHOW COLUMNS FROM totalsalesandmetrics;
